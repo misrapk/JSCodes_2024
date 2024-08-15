3f2487e1ceb21e87e6293d091d4defec8558ca88
Peeyush Kant Misra <pkmisra1999@gmail.com>
2024-08-15 20:25:15 +0530

    day1

---
+++ 01Basics/01variables.js
@@ -0,0 +1,6 @@
+const accountID = 1234
+let accountEmail = "pk@gmail.com"
+var accountPassword = "12345"
+
+
+//prefer not to use var; use only "let" or "const"
---
+++ 01Basics/02dataTypes.js
@@ -0,0 +1,6 @@
+"use stricts"; //use all JS code as newer Version throught the file.
+
+
+console.log(3+3);
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
