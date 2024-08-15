7a93d35f550be67ac98693aaab1dbc7de5694a12
Peeyush Kant Misra <pkmisra1999@gmail.com>
2024-08-15 20:36:14 +0530

    Day 1 more

---
+++ 01Basics/03conversionOperation.js
@@ -0,0 +1,17 @@
+// let score = 89
+// let score = "qwerty"
+// let score = undefined
+let score = null
+
+console.log(typeof score)
+
+let scoreInNumber = Number(score)
+console.log(typeof scoreInNumber)
+console.log(scoreInNumber)
+
+
+// number ==> number
+// "string" ==> NAN
+// "boolean" ==> 0(false), 1(true)
+
+
